de207abbd788bf809426a7ef14e9d9cd8447d573
RTMN-4 <ratchapol.t@kkumail.com>
2025-09-29 23:38:42 +0700

    indexNew

--- platformer#3d/main.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@onready var label: Label = $CanvasLayer/Label
-
-func _ready():
-	# แสดงข้อความตลอดเวลา
-	label.text = "จัดทำโดย นายรัชพล ธนาปฏิ 663380292-4"
-	label.visible = true
